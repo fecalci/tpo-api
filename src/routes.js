1fcebf510527314c85492bc7cbd4ef3bd9a17c63
fecalci <fcalciano@uade.edu.ar>
2021-09-24 03:17:33 -0300

    Commit before first deliver, added vaccineRegister with Modal

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import Main from "./components/main";
+// import Main from "./components/main";
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
-import LineChart from './components/Graphs/LineChar'
-// import Main from './components/navbar'
+import LineChart from './components/Graphs/LineChar';
+import VaccineRegister from './components/VaccineRegister';
 
 const Routes= () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Main} />
+            <Route exact path = "/" component={VaccineRegister} />
+            {/* <Route exact path="/" component={Main} /> */}
             <Route exact path="/login" component={Login} /> 
             <Route exact path="/signup" component={SignUp} /> 
             <Route exact path="/char" component={LineChart} /> 
